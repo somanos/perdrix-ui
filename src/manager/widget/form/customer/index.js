490cc5d0808234c93d5515fa609248b6ac4940e5
Somanos Sar <somanos@drumee.com>
2025-03-15 08:54:56 +0700

    Rework form/customer

--- src/manager/widget/form/customer/index.js
+++ src/manager/widget/form/customer/index.js
@@ -1,6 +1,14 @@
 
 const __form = require('..');
 class __form_customer extends __form {
+
+  constructor(...args) {
+    super(...args);
+    this.searchCustomer = this.searchCustomer.bind(this);
+    this.searchLocation = this.searchLocation.bind(this);
+  }
+
+
   /**
    * 
    */
@@ -11,13 +19,8 @@ class __form_customer extends __form {
     this.model.atLeast({
       type: 'company'
     })
-    this._api = {
-      service: "perdrix.customer_search",
-    }
-    this._timer = {
-      location: 0
-    }
-    this._pending = {};
+    this._data = {}
+    this._timer = {}
   }
 
   /**
@@ -35,122 +38,127 @@ class __form_customer extends __form {
   /**
    * 
    */
+  async feedList(api, itemsOpt, will, wont) {
+    let list = await this.ensurePart(_a.list);
+    list.mset({ api, itemsOpt });
+    list.restart();
+    let footer = await this.ensurePart(_a.footer);
+    footer.el.dataset.state = 1;
+    list.once(_e.data, async (data) => {
+      this.debug("AAA:66 GOT DATA", data)
+      will(data);
+    })
+    list.once(_e.eod, async () => {
+      this.debug("AAA:70 GOT EOD", data)
+      will(data);
+    });
+    list.once(_e.error, async () => {
+      this.debug("AAA:104 ERROR", list.isEmpty())
+      will([]);
+    });
+  }
+
+  /**
+   * 
+   */
   searchCustomer(cmd) {
-    if (this._pending.customer) return;
-    if (!cmd || !cmd.getValue) return;
     let words = cmd.getValue();
-    if (!words || !words.length) return;
-    if (this._pending.customer == words) return;
-    this._pending.customer = words;
-    this._api = {
+    let api = {
       service: "perdrix.customer_search",
       words,
       type: this.mget(_a.type)
     };
-    this.ensurePart(_a.list).then(async (list) => {
-      let itemsOpt = {
-        kind: 'customer_item',
-        origin: 'searchbox',
-        service: null
-      }
-      list.mset({ api: this._api, itemsOpt });
-      list.restart();
-      let footer = await this.ensurePart(_a.footer);
-      footer.el.dataset.state = 1;
-      list.once(_e.data, async (data) => {
-        this._pending.customer = null;
-      })
-      list.once(_e.eod, async () => {
-        this._pending.customer = null;
-      });
-      list.once(_e.error, async () => {
-        this._pending.customer = null;
-      });
+    let itemsOpt = {
+      kind: 'customer_item',
+      origin: 'searchbox',
+      service: null
+    }
+    return new Promise((will, wont) => {
+      if (!words || !words.length) return will();
+      this.feedList(api, itemsOpt, will, wont)
     })
   }
 
   /**
    * 
    */
-  async searchLocation(words) {
-    //if (this._pending.location) return;
-    this._api = {
+  async searchLocation(cmd) {
+    let words = cmd.getValue() || "";
+    let { length } = words.split(/[ ,]+/)
+    let api = {
       service: "perdrix.search_location",
       words,
     };
-    this._pending.location = words;
-
-    this.ensurePart(_a.list).then(async (list) => {
-      let itemsOpt = {
-        kind: 'location_item',
-        service: 'select-address'
-      }
-      list.mset({ api: this._api, itemsOpt });
-      list.restart();
-      let footer = await this.ensurePart(_a.footer);
-      footer.el.dataset.state = 1;
-      list.once(_e.data, async (data) => {
-        this.debug("AAA:100", list.isEmpty(), list, footer)
-        this._pending.location = null;
-      })
-      list.once(_e.eod, async () => {
-        this._pending.location = null;
-        this.debug("AAA:1006", list.isEmpty(), list, footer)
-      });
-      list.once(_e.error, async () => {
-        this._pending.location = null;
-        this.debug("AAA:1111", list.isEmpty(), list, footer)
-        list.clear()
-      });
-
+    let itemsOpt = {
+      kind: 'location_item',
+      service: 'select-address'
+    }
+    return new Promise(async (will, wont) => {
+      if (length <= 2) return will(null);
+      this.feedList(api, itemsOpt, will, wont)
     })
   }
 
   /**
    * 
    */
-  throtleLocation(cmd) {
-    this.debug("AAA:121", cmd, this._pending.location, this._locationCompleted, this._currentWords, this._timer.location)
-    if (this._locationCompleted) return;
-    if (!cmd || !cmd.getValue) return;
-    if (this._timer.location) return;
-    let words = cmd.getValue() || "";
-    let { length } = words.split(/[ ,]+/)
-    this.debug("AAA:129", cmd, length, words, this._pending.location, this._locationCompleted, this._currentWords)
-    if (length <= 2) return;
-    if (this._currentWords == words) return;
-    this._timer.location = setTimeout(() => {
-      words = cmd.getValue() || "";
-      this._currentWords = words;
-      this.searchLocation(words);
-      this._timer.location = 0;
-      if (this._pending.location && this._pending.location != this._currentWords) {
-        this.searchLocation(words);
-      }
-    }, 1000)
-  }
+  // throtleLocation(cmd) {
+  //   this.debug("AAA:121", cmd, this._pending.location, this._locationCompleted, this._currentWords, this._timer.location)
+  //   if (this._locationCompleted) return;
+  //   if (!cmd || !cmd.getValue) return;
+  //   if (this._timer.location) return;
+  //   let words = cmd.getValue() || "";
+  //   let { length } = words.split(/[ ,]+/)
+  //   this.debug("AAA:129", cmd, length, words, this._pending.location, this._locationCompleted, this._currentWords)
+  //   if (length <= 2) return;
+  //   if (this._currentWords == words) return;
+  //   this._timer.location = setTimeout(() => {
+  //     words = cmd.getValue() || "";
+  //     this._currentWords = words;
+  //     this.searchLocation(words);
+  //     this._timer.location = 0;
+  //     if (this._pending.location && this._pending.location != this._currentWords) {
+  //       this.searchLocation(words);
+  //     }
+  //   }, 1000)
+  // }
 
   /**
    * 
    */
-  throtleCustomer(cmd) {
-    if (!cmd || !cmd.getValue) return;
-    if (this._timer.customer) return;
-    let words = cmd.getValue() || "";
-    let { length } = words.split(/[ ,]+/)
-    if (length <= 2) return;
-    if (this._currentWords == words) return;
-    this._timer.customer = setTimeout(() => {
-      words = cmd.getValue() || "";
-      this._currentWords = words;
-      this.debug("AAA:121", cmd, length, this._pending.customer, this._currentWords)
-      this.searchCustomer(words);
-      this._timer.customer = 0;
-      if (this._pending.customer && this._pending.customer != this._currentWords) {
-        this.searchCustomer(words);
-      }
-    }, 1000)
+  // throtleCustomer(cmd) {
+  //   if (!cmd || !cmd.getValue) return;
+  //   if (this._timer.customer) return;
+  //   let words = cmd.getValue() || "";
+  //   let { length } = words.split(/[ ,]+/)
+  //   if (length <= 2) return;
+  //   if (this._currentWords == words) return;
+  //   this._timer.customer = setTimeout(() => {
+  //     words = cmd.getValue() || "";
+  //     this._currentWords = words;
+  //     this.debug("AAA:121", cmd, length, this._pending.customer, this._currentWords)
+  //     this.searchCustomer(words);
+  //     this._timer.customer = 0;
+  //     if (this._pending.customer && this._pending.customer != this._currentWords) {
+  //       this.searchCustomer(words);
+  //     }
+  //   }, 1000)
+  // }
+
+  /**
+    * 
+    */
+  throtle(cmd) {
+    return new Promise((will, wont) => {
+      if (!cmd || !cmd.getValue) return;
+      if (this._timer[cmd.cid]) return;
+      this._timer[cmd.cid] = setTimeout(async () => {
+        await will(cmd);
+        this._timer[cmd.cid] = null;
+      }, 1000)
+    })
   }
+
 
   /**
   * 
@@ -159,7 +167,8 @@ class __form_customer extends __form {
     this.debug("AAA:158", cmd)
     this.ensurePart("menu-trigger").then((p) => {
       this.debug("AAA:158", this, p, cmd);
-      p.setLabel(cmd.mget(_a.label))
+      p.setLabel(cmd.mget(_a.label));
+      this._data[cmd.mget(_a.name)] = cmd.mget(_a.value)
     })
   }
 
@@ -189,7 +198,7 @@ class __form_customer extends __form {
    * 
    */
   createCustomer() {
-    let args= this.getData();
+    let args = this.getData();
     this.debug("AAA:201", args)
     this.postService("perdrix.customer_create", args).then((data) => {
       this.debug("AAAA:202", data)
@@ -224,11 +233,11 @@ class __form_customer extends __form {
       case _a.input:
         switch (cmd.mget(_a.name)) {
           case _a.name:
-            this.searchCustomer(cmd);
+          case _a.lastname:
+            this.throtle(cmd).then(this.searchCustomer);
             break;
           case _a.location:
-            this._locationCompleted = 0;
-            this.throtleLocation(cmd);
+            this.throtle(cmd).then(this.searchLocation);
             break;
         }
         break;
